b094c22741e45d68b82ecb429f5a2635d95e08fa
Mayronn Gomes <mayronngomes@alu.ufc.br>
2024-12-12 18:55:59 -0300

    Adicionado a tela home

---
+++ src/app/Login/page.js
@@ -0,0 +1,34 @@
+"use client";
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+    const router = useRouter();
+
+    const goToHome = (event) => {
+        event.preventDefault();
+        router.push('/Home');
+      };
+
+    return (
+        <div className='flex w-full h-screen bg-[url("/images/login.png")] bg-cover bg-center justify-center items-center'>
+            <div className="bg-[#061E5A] text-white rounded-lg p-8 w-1/4">
+                <h1 className="text-4xl font-bold mb-6">Seja bem-vindo(a)</h1>
+                <form onSubmit={goToHome}>
+                    <div className="mb-4">
+                        <label className="block text-base mb-2" htmlFor="matricula">Matrícula ou Siape</label>
+                        <input className="w-full p-2 border border-gray-300 rounded" type="text" id="matricula" placeholder="Entre com sua matrícula/siape"></input>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-base mb-2" htmlFor="senha">Senha</label>
+                        <input className="w-full p-2 border border-gray-300 rounded" type="password" id="senha" placeholder="Entre com sua senha"></input>
+                    </div>
+                    <button className="w-full bg-white text-black py-2 rounded font-bold" type="submit">Entrar</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
